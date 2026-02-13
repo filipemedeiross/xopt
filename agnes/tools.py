b563ab35273a28b207997735e3841deed2d463ae
Filipe Medeiros <filipemedeiros2013@gmail.com>
2026-02-13 17:24:59 -0300

    New helper functions to transform list to matrix and normalize linkage

---
+++ agnes/tools.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+from scipy.sparse import csr_matrix
+
+
+def list2matrix(adj_list, n, l):
+    rows = []
+    cols = []
+    data = []
+
+    for u, adj in enumerate(adj_list):
+        for v in adj:
+            rows.append(u)
+            cols.append(v)
+            data.append(1)
+
+    matrix = (data, (rows, cols))
+
+    return csr_matrix(matrix, shape=(n, l), dtype='uint8')
+
+
+def normalize_linkage(L):
+    canonical = L       .copy ()
+    children  = L[:, :2].ravel()
+
+    argsort = np.argsort(children)
+    indices = np.argsort(argsort )
+
+    canonical[:, :2] = indices.reshape(children.shape)
+
+    return canonical
