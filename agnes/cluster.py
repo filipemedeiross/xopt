c6917b26d79f227281273f0c95909844f406c148
Filipe Medeiros <filipemedeiros2013@gmail.com>
2026-02-16 10:51:21 -0300

    New types for attributes and improving eficiency in cluster class

--- agnes/cluster.py
+++ agnes/cluster.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from typing       import List
 from dataclasses  import dataclass
 from scipy.sparse import csr_matrix
 
@@ -8,7 +7,7 @@ from scipy.sparse import csr_matrix
 @dataclass(slots=True, eq=False)
 class Cluster:
     id: int
-    v : List[int]   # vertices in the cluster
+    v : set[int]    # vertices in the cluster
     l : csr_matrix  # shared-resource intersection
 
     def __post_init__(self) -> None:
@@ -18,16 +17,8 @@ class Cluster:
         if self.l.shape[0] != 1:
             raise ValueError("l must have shape (1, L)")
 
-    def __add__(self, other: Cluster) -> List[int]:
-        seen = set()
-        out  = []
-
-        for x in self.v + other.v:
-            if x not in seen:
-                seen.add   (x)
-                out .append(x)
-
-        return out
+    def __add__(self, other: Cluster) -> set[int]:
+        return self.v | other.v
 
     def __and__(self, other: Cluster) -> csr_matrix:
         return self.l.multiply(other.l)
@@ -39,7 +30,7 @@ class Cluster:
         if self.l.shape[1] != other.l.shape[1]:
             raise ValueError("Clusters must share the same universe size L")
 
-        return int((self.l @ other.l.T).toarray()[0, 0])
+        return int((self.l @ other.l.T)[0, 0])
 
     def similarity(self, other: Cluster) -> int:
         return self @ other
