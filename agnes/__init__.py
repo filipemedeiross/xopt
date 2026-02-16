a3dcc983f52b4d929036ebeae12f7a99e012220b
Filipe Medeiros <filipemedeiros2013@gmail.com>
2026-02-16 19:18:41 -0300

    First contract to agnes module

---
+++ agnes/__init__.py
@@ -0,0 +1,12 @@
+from .agnes      import agnes
+from .graph      import BiGraph
+from .cluster    import UnionFind
+from .dendrogram import dendrogram
+
+
+__all__ = [
+    agnes     ,
+    BiGraph   ,
+    UnionFind ,
+    dendrogram,
+]
