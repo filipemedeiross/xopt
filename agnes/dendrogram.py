0cda38019113677ccc9bd05347dce5132f528d53
Filipe Medeiros <filipemedeiros2013@gmail.com>
2026-02-18 18:14:04 -0300

    Second version of dendrogram function

--- agnes/dendrogram.py
+++ agnes/dendrogram.py
@@ -5,13 +5,13 @@ import scipy.cluster.hierarchy as scipy
 from .tools import normalize_linkage
 
 
-def dendrogram(L, n, l, format_labels=None):
+def dendrogram(Ls, n, l, format_labels=None):
     '''
     Recebe  -> lista de linkages e os valores de N e L para a instância
     Retorna -> lista de objetos Figure, cada qual representando um cluster
     '''
-    if isinstance(L, np.ndarray):
-        L = [L]
+    if isinstance(Ls, np.ndarray):
+        Ls = [Ls]
 
     if format_labels:
         label = lambda x: format_labels[int(x)]
@@ -19,18 +19,27 @@ def dendrogram(L, n, l, format_labels=None):
         label = lambda x: f'X{int(x)}'
 
     figs = []
-    for c in range(len(L)):
-        indices = L[c][:, :2].flatten()
-        labels  = [label(i) for i in np.sort(indices[np.where(indices < n)])]
 
-        x_min = L[c][:, 2].min() - 1
-        x_max = L[c][:, 2].max() + 1
+    for L in Ls:
+        linkage = normalize_linkage(L)
+
+        indices = L[:, :2].copy   ()
+        flat    = indices .flatten()
+
+        x_min  = indices.min() - 1
+        x_max  = indices.max() + 1
+        labels = [
+            label(i)
+            for i in np.sort(flat[flat < n])
+        ]
 
         fig, _ = plt.subplots()
-        scipy.dendrogram(normalize_linkage(L[c]),
-                         labels=labels,
+
+        scipy.dendrogram(linkage,
+                         labels     =labels ,
                          orientation='right')
-        plt.xlim((x_min, x_max))
+
+        plt.xlim  ((x_min, x_max))
         plt.xticks(plt.xticks()[0], l - plt.xticks()[0])
 
         figs.append(fig)
