b74d1c993d00c49ff1dfc291a817d07f2dd883ce
Filipe Medeiros <filipemedeiros2013@gmail.com>
2026-02-20 18:06:44 -0300

    New dunder init in agnes module

--- agnes/__init__.py
+++ agnes/__init__.py
@@ -2,11 +2,13 @@ from .agnes      import agnes
 from .graph      import BiGraph
 from .cluster    import UnionFind
 from .dendrogram import dendrogram
+from .tools      import vertex2linkage
 
 
 __all__ = [
-    agnes     ,
-    BiGraph   ,
-    UnionFind ,
-    dendrogram,
+    agnes         ,
+    BiGraph       ,
+    UnionFind     ,
+    dendrogram    ,
+    vertex2linkage,
 ]
